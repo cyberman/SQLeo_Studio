72ff0360d135fac939b7206c0d46fa8474eb9267
Paweł Salawa <pawelsalawa@gmail.com>
2026-05-08 20:40:53 +0200

    New translations pdfexport.ts (German)
    [ci skip]

--- Plugins/PdfExport/translations/PdfExport_de_DE.ts
+++ Plugins/PdfExport/translations/PdfExport_de_DE.ts
@@ -4,11 +4,6 @@
   <context>
     <name>PdfExport</name>
     <message>
-      <location filename="../pdfexport.cpp" line="33"/>
-      <source>SQLiteStudio v%1</source>
-      <translation>SQLiteStudio v%1</translation>
-    </message>
-    <message>
       <location filename="../pdfexport.cpp" line="66"/>
       <source>SQL query results</source>
       <translation>SQL Abfragenergebnis</translation>
@@ -23,6 +18,11 @@
       <location filename="../pdfexport.cpp" line="153"/>
       <source>Table: %1</source>
       <translation>Tabelle: %1</translation>
+    </message>
+    <message>
+      <location filename="../pdfexport.cpp" line="33"/>
+      <source>Letos v%1</source>
+      <translation type="unfinished">Letos v%1</translation>
     </message>
     <message>
       <location filename="../pdfexport.cpp" line="104"/>
@@ -160,15 +160,15 @@
       <translation>View: %1</translation>
     </message>
     <message>
+      <location filename="../pdfexport.cpp" line="710"/>
+      <source>Document generated with Letos v%1</source>
+      <translation type="unfinished">Document generated with Letos v%1</translation>
+    </message>
+    <message>
       <location filename="../pdfexport.cpp" line="69"/>
       <location filename="../pdfexport.cpp" line="335"/>
       <source>Query:</source>
       <translation>Abfrage:</translation>
-    </message>
-    <message>
-      <location filename="../pdfexport.cpp" line="710"/>
-      <source>Document generated with SQLiteStudio v%1</source>
-      <translation>Dokument mit SQLiteStudio v%1 generiert</translation>
     </message>
   </context>
   <context>
